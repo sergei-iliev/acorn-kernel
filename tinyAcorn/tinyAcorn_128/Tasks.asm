46162db314abd1dc26f4a1f2a003fc2a5ee4a9a5
Sergey Iliev <sergei_iliev@yahoo.com>
2019-03-21 10:03:15 +0200

    split tiny cores

--- tinyAcorn/tinyAcorn_128/Tasks.asm
+++ tinyAcorn/tinyAcorn_128/Tasks.asm
@@ -1,9 +1,12 @@
+/*
+BARE BONE app based on 3 tasks
+Look at https://github.com/sergei-iliev/acorn-kernel-samples 
+for working demos
+*/
 
 .cseg
 
 
-
-;Pulse Width Modulation (Diming a LED) TASK
 
 ;equal to task's ID
 .set dpc_index1=1
@@ -13,7 +16,7 @@ Task_1:
    
 main1:
 nop
-nop
+_SUSPEND_TASK
 rjmp main1  
 ret
 
@@ -32,9 +35,8 @@ ret
 .set dpc_index3=3
 Task_3:	
 nop
-_SLEEP_TASK 3
 main3:
 nop
-_SLEEP_TASK 13
+_SLEEP_TASK 3
 rjmp main3  	
 ret
